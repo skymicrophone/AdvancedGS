0bb74eaffb614654720a0ee60fdee4967c2da11e
skymicrophone <531135228@qq.com>
2025-08-25 15:53:01 +0800

    AdvancedGS

--- CurrentCoding/mbtifan(2).py
+++ CurrentCoding/mbtifan(2).py
@@ -8,4 +8,4 @@ for i0 in range(0,1):
     aaa0.encoding="UTF-8";xxx0=etree.HTML(aaa0.text);ash0=xxx0.xpath("//*[@class='comment']/div/div[2]/div[1]")
     for art0 in ash0:
         ART=art0.xpath("text()");dict0["コメント"+ART[0]]=ART[2]
-print(dict0);SFO(r"C:\Users\53113\Desktop\mbtifan.json").FWriter([json.dumps(dict0,ensure_ascii=False)],'w','UTF-8')
+print(dict0)#;SFO(r"C:\Users\53113\Desktop\mbtifan.json").FWriter([json.dumps(dict0,ensure_ascii=False)],'w','UTF-8')
