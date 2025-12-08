fbd269496d06d4d80dbeda29cc7c055bdd86d49b
skymicrophone <531135228@qq.com>
2025-12-08 20:34:05 +0800

    AniDB

--- CurrentCoding/anidb.py
+++ CurrentCoding/anidb.py
@@ -11,8 +11,8 @@ for i0 in range(0,10):
         exec_art+="\n\ttry:\n\t\tif "+exec_ad+"\"Image\":\n\t\t\tart0[\"URL(IMG)\"]=y0.xpath(\"a/picture/img/@src\")[0]"
         exec_art+="\n\t\telif "+exec_ad+"\"Title\":\n\t\t\tart0[\"URL\"]=\"https://anidb.net\"+y0.xpath(\"a/@href\")[0];"
         exec_art+="art0[\"Title\"]=y0.xpath(\"a/text()\")[0]\n\t\telif "+exec_ad+"\"Award\":\n\t\t\tif "
-        exec_art+="y0.xpath(\"span/span/text()\")!=[]:\n\t\t\t\tart0[\"Award\"]=\", \".join(y0.xpath(\"span/span/text()\"))"
-        exec_art+="\n\t\t\telse:\n\t\t\t\tart0[\"Award\"]=\"\"\n\t\telif "+exec_ad+"\"Eps\":\n\t\t\tart0[\"Eps\"]="
+        exec_art+="y0.xpath(\"span/span/text()\")!=[]:\n\t\t\t\tart0[\"Awards\"]=\", \".join(y0.xpath(\"span/span/text()\"))"
+        exec_art+="\n\t\t\telse:\n\t\t\t\tart0[\"Awards\"]=\"\"\n\t\telif "+exec_ad+"\"Eps\":\n\t\t\tart0[\"Eps\"]="
         exec_art+="y0.xpath(\"string(.)\").replace(\"\\n\",\"\").replace(\"\\t\",\"\")\n\t\telif "+exec_ad+"\"Weight\":\n\t\t\t"
         exec_art+="art0[\"Weight\"]=y0.xpath(\"span/@title\")[0]\n\t\telse:\n\t\t\tart0[y0.xpath(\"@data-label\")[0]]="
         exec(exec_art+"y0.xpath(\"text()\")[0].replace(\"\\n\",\"\").replace(\"\\t\",\"\")\n\texcept:\n\t\tpass");kkk0.append(art0)
@@ -20,3 +20,4 @@ for i0 in range(0,10):
 pddf0=pd.DataFrame(kkk0);TM(pddf0).TkinterTable('AniDB','750x450',20,15,'n')
 #pddf0.to_csv(r"C:\Users\53113\Desktop\anidb0.csv",index=False)
 #pddf0.to_excel(r"C:\Users\53113\Desktop\anidb0.xlsx",index=False)
+
